3161d2073aa4c59eb78d40c0cc4b06acc123eab8
Pavan Kumar Vadupu Lakshman Manikya <vlmpk2003@gmail.com>
2025-02-22 00:07:26 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'userPoolIdValue', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'userPoolClientIdValue', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'userRegionValue' // e.g. us-east-2
+        userPoolId: 'us-west-2_6AzQePpVA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7pbin5hesgekanjac0vc11q1pf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrlRest: 'userRestURL',// e.g. https://44fml92a5k.execute-api.us-east-1.amazonaws.com/Prod
-        invokeUrlWebS: 'userWebSURL'//e.g. wss://gzhlhvjisi.execute-api.us-east-1.amazonaws.com/Prod
+        invokeUrlRest: 'https://rdbblt94z8.execute-api.us-west-2.amazonaws.com/Prod',// e.g. https://44fml92a5k.execute-api.us-east-1.amazonaws.com/Prod
+        invokeUrlWebS: 'wss://irrj8kw769.execute-api.us-west-2.amazonaws.com/Prod'//e.g. wss://gzhlhvjisi.execute-api.us-east-1.amazonaws.com/Prod
     }
 };
